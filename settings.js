a47006308307b975f49c707f010b73793e2e5feb
healthebooks <47319895+healthebooks@users.noreply.github.com>
2022-08-06 16:03:54 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@ const infuraId = "1d5e6d029a604a09870214e2a3801cb2"     // Infuria ID | https://
 const moralisApi = "FQhPZwDeL9pDYzQqLOT5DmQPOLoRBJI3qdOjlnDdshaSO0auU7Dr7SD1OnZVKxR0"    // x-api-key | https://moralis.io/ | For NFTs
 
 const collectionInfo = {
-    name: "Yin Yang Gang NFT",
+    name: "Yin Yang Gang Pre sale",
     title: "YYG MINT", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
-    date: "08.06.2022",
+    date: "08.07.2022",
     socialMedia: {
         discord: "https://discord.com/invite/yyg",
         twitter: "https://twitter.com/projectyingang",
